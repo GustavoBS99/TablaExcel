ef0fb4ddcc8a6eecbcdd6d663d16f1d0672729db
GustavoBS99 <42751966+GustavoBS99@users.noreply.github.com>
2018-12-05 09:24:05 -0800

    Add files via upload

---
+++ SQLtexto/src/textoe.java
@@ -0,0 +1,149 @@
+
+
+import java.io.*;
+import java.util.*;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class textoe {
+
+    public static void main(String[] args) throws Exception {
+
+        File arch = new File("C:\\Users\\Carolina\\Desktop\\SQL.txt");
+        BufferedReader br = new BufferedReader(new FileReader(arch));
+        String txt;
+        String t = "";
+        String ts = "";
+        String ord = "";
+        while ((txt = br.readLine()) != null) {
+            ord = ord + " " + txt;
+            if (txt.contains(".") && txt.contains("com") && txt.contains("=")) {
+                t = t + " " + txt.substring(txt.indexOf(".") + 1, txt.indexOf("=") - 1) + " "
+                        + txt.substring(txt.lastIndexOf(".") + 1, txt.length());
+                ts = ts + " " + txt.substring(3, txt.indexOf(".")) + " "
+                        + txt.substring(txt.indexOf("=") + 1, txt.lastIndexOf("."));
+            } else if (txt.contains(".") && !txt.endsWith(",")) {
+                if (txt.contains("WHERE")) {
+                    t = t + " " + txt.substring(txt.indexOf(".") + 1, txt.indexOf("=") - 1);
+                    ts = ts + " " + txt.substring(txt.indexOf(" "), txt.indexOf("."));
+                } else if (txt.contains("ORDER")) {
+
+                } else {
+                    ts = ts + " " + txt.substring(0, txt.indexOf("."));
+                    t = t + " " + txt.substring(txt.indexOf('.') + 1, txt.length());
+                }
+            } else if (txt.contains(".") && txt.contains(",")) {
+                t = t + " " + txt.substring(txt.indexOf('.') + 1, txt.length() - 1);
+                ts = ts + " " + txt.substring(0, txt.indexOf("."));
+
+            }
+        }
+        StringTokenizer st = new StringTokenizer(t);
+        StringTokenizer st2 = new StringTokenizer(ts);
+        int tm = st2.countTokens();
+        String[][] txtar = new String[tm + 1][2];
+
+        for (int j = 1; j < txtar.length; j++) {
+            txtar[j][1] = st.nextToken();
+            txtar[j][0] = st2.nextToken();
+            System.out.println(txtar[j][0] + "   " + txtar[j][1]);
+        }
+        for (int j = 1; j < txtar.length; j++) {
+            if (txtar[j][0].contains("stu")) {
+                txtar[j][0] = "Students";
+            }
+            if (txtar[j][0].contains("com")) {
+                txtar[j][0] = "Comments";
+            }
+        }
+
+        System.out.println(ord);
+        if (ord.contains("ASC")) {
+            txtar[0][0] = "zTableName";
+            txtar[0][1] = "zColumnName";
+            Arrays.sort(txtar, new Comparator<String[]>() {
+                @Override
+                public int compare(String[] second, String[] first) {
+                    // compare the first element
+                    int comparedTo = first[0].compareTo(second[0]);
+                    // if the first element is same (result is 0), compare the second element
+                    if (comparedTo == 0) {
+                        return first[1].compareTo(second[1]);
+                    } else {
+                        return comparedTo;
+                    }
+                }
+            });
+            txtar[0][0] = txtar[0][0].substring(1);
+            txtar[0][1] = txtar[0][1].substring(1);
+        }
+
+        if (ord.contains("DST")) {
+            txtar[0][0] = "0TableName";
+            txtar[0][1] = "0ColumnName";
+            Arrays.sort(txtar, new Comparator<String[]>() {
+                @Override
+                public int compare(String[] first, String[] second) {
+                    // compare the first element
+                    int comparedTo = first[0].compareTo(second[0]);
+                    // if the first element is same (result is 0), compare the second element
+                    if (comparedTo == 0) {
+                        return first[1].compareTo(second[1]);
+                    } else {
+                        return comparedTo;
+                    }
+                }
+            });
+            txtar[0][0] = txtar[0][0].substring(1);
+            txtar[0][1] = txtar[0][1].substring(1);
+        }
+        crearExcel("C:\\Users\\Carolina\\Desktop\\SQL.xls", "SQL", txtar);
+    }
+
+    public static void crearExcel(String narch, String nt, String[][] txt) throws FileNotFoundException, IOException {
+        Workbook wb = new HSSFWorkbook();
+        FileOutputStream fo = new FileOutputStream(narch);
+        Sheet h = wb.createSheet(nt);
+
+        CellStyle style = wb.createCellStyle();
+        
+        
+        
+        Font font = wb.createFont();
+        font.setFontName("Arial");
+        font.setBold(true);
+        font.setColor((short) 0);
+        font.setFontHeightInPoints((short) 12);
+        style.setFont(font);
+        style.setBorderBottom(BorderStyle.THIN);
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderRight(BorderStyle.THIN);
+        style.setBorderLeft(BorderStyle.THIN);
+
+        
+        
+        Row[] f = new Row[txt.length];
+        Cell[][] c = new Cell[f.length][];
+        for (int i = 0; i < f.length; i++) {
+            f[i] = h.createRow(i);
+            c[i] = new Cell[txt[i].length];
+            for (int j = 0; j < c[i].length; j++) {
+                c[i][j] = f[i].createCell(j);
+                c[i][j].setCellValue(txt[i][j]);
+                c[i][j].setCellStyle(style);
+            }
+        }
+
+        h.autoSizeColumn(0);
+        h.autoSizeColumn(1);
+
+        wb.write(fo);
+
+        fo.close();
+        System.out.println("Se ha exportado de forma correcta.");
+    }
+
+}
